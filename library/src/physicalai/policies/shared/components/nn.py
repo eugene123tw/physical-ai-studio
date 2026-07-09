e6bb5b79d864fbdc1403bdc67abb830817ad49d1
Eugene Liu <eugene.liu@intel.com>
2026-07-09 09:57:08 +0200

    fix: Update weight selection in CategorySpecificLinear to use full weight matrix

--- library/src/physicalai/policies/shared/components/nn.py
+++ library/src/physicalai/policies/shared/components/nn.py
@@ -112,7 +112,7 @@ class CategorySpecificLinear(nn.Module):
             Output tensor of shape (B, T, hidden_dim).
         """
         max_action_dim = x.shape[-1]
-        selected_w = self.W[cat_ids][:, :max_action_dim, :]
+        selected_w = self.W[cat_ids]
         selected_b = self.b[cat_ids]
         return torch.bmm(x, selected_w) + selected_b.unsqueeze(1)
 
